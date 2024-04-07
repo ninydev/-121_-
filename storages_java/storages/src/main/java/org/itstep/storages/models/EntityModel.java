48158fcbc5c032e55ff98572dfb34d32f0be5442
Oleksandr Nykytin <keeper@ninydev.com>
2024-04-07 16:52:52 +0300

    First

---
+++ storages_java/storages/src/main/java/org/itstep/storages/models/EntityModel.java
@@ -0,0 +1,33 @@
+package org.itstep.storages.models;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "entities")
+public class EntityModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Id", example = "1")
+    private Long id;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Schema(description = "Created At", example = "10/10/2024 13:13:00")
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Schema(description = "Updated At", example = "10/10/2024 13:13:00")
+    @Column(name = "updated_at", nullable = false)
+    private Date updatedAt;
+}
