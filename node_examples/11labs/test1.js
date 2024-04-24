10adde8cbe7ca134ad57ca4b188017ef98d0679a
Oleksandr Nykytin <keeper@ninydev.com>
2024-04-24 20:20:50 +0300

    init

--- node_examples/11labs/test1.js
+++ node_examples/11labs/test1.js
@@ -1,13 +1,55 @@
+const fs = require('fs');
+require('dotenv').config()
+filePath= './res.mp3'
+let text = "Hello"
+let voice_settings = {
+    stability: 0.5,
+    similarity_boost:0.5,
+    style: 0.5,
+    use_speaker_boost: false
+}
+
+let body = {text, voice_settings}
+
+console.log(body)
+
 const options = {
     method: 'POST',
     headers: {
-        'xi-api-key': 'a2480aeb2570c2ccfe0e6e718b5ba99f',
+        'xi-api-key': process.env.LABS_KEY,
         'Content-Type': 'application/json'
     },
-    body: '{"text":"В очах твоїх я бачу край небес, Там вітер вінок зірок розплітає. Ти, моя дівчино, як тополь зелений, Що серце моє легко забуває.","voice_settings":{"stability":0.123,"similarity_boost":0.123,"style":0.123,"use_speaker_boost":true}}'
+    body: JSON.stringify(body)
 };
 
+
+console.log(options)
+
 fetch('https://api.elevenlabs.io/v1/text-to-speech/RzI0GdCJKAD9xMkuY0j8', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => {
+        // вместо response.json() и других методов
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const reader = response.body.getReader();
+        const fileStream = fs.createWriteStream(filePath);
+
+        // Функция для рекурсивного чтения данных из потока и записи в файл
+        function pump() {
+            reader.read().then(({ done, value }) => {
+                if (done) {
+                    fileStream.end();
+                    return;
+                }
+                fileStream.write(Buffer.from(value));
+                pump();
+            }).catch((err) => {
+                console.log("pump err")
+                console.log (err)
+            });
+        }
+
+        pump(); // Запускаем процесс чтения данных и записи в файл
+
+    })
     .catch(err => console.error(err));
